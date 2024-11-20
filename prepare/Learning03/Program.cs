212f6010374a9032681fd85959610fa48fea4a5a
BYUI-surundu1 <francissurundu1@gmail.com>
2024-11-20 16:47:19 +0300

    Fraction work

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction myFraction = new Fraction(3/4);
+        Console.WriteLine(myFraction.GetFractionString());
+        Console.WriteLine(myFraction.GetValue());
+        Console.WriteLine(myFraction.ToString());
     }
+
 }
