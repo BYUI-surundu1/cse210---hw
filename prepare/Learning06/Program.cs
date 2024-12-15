b8a44f1c352d5a7d4818e760d948c67778368c6d
BYUI-surundu1 <francissurundu1@gmail.com>
2024-12-15 13:02:51 +0300

    week 6 prepare assignment

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Blue", 3, 5),
+            new Circle("Green", 2.5)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.Color}, Area: {shape.GetArea():F2}");
+        }
     }
 }
